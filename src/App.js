3764a4cbd3e3677d2f1cd00097827d6860831a05
Kevin Van Leer <kevin.vanleer@gmail.com>
2020-11-28 12:30:18 -0600

    Added privacy policy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} alt="logo" />
-        <p>Cyclotrack</p>
+        <h1>Cyclotrack</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -16,6 +16,11 @@ function App() {
         >
           Download Cyclotrack from the Google Play Store
         </a>
+        <h3>Privacy Policy</h3>
+        All data collected by Cyclotrack is stored on your device. No data is
+        transmitted from your device to the developer of Cyclotrack or any third
+        party. If you choose to integrate Cyclotrack with a third party app, it
+        is your responsibility to protect your data.
       </header>
     </div>
   );
